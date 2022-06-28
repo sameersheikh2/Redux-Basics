0425e85c20da5380dbae41e7f18cb35ecd9298d9
sameersheikh2 <sameersheikh819@gmail.com>
2022-06-28 22:10:23 +0530

    provide redux store in our components

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,11 @@
 import classes from './Counter.module.css';
+import { useSelector } from 'react-redux';
+
 
 const Counter = () => {
+
+const counter = useSelector(state=> state.counter)
+
   const toggleCounterHandler = () => {};
 
   return (
