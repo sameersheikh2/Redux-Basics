7757bcfff537fe26cac9ff9aa1ed8736f527683a
sameersheikh2 <sameersheikh819@gmail.com>
2022-06-30 15:53:26 +0530

    added redux store to header login and logout for

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+
 import Counter from './components/Counter';
+import Header from './components/Header';
+import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
 
 
 function App() {
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
+
   return (
-    <Counter />
+    <Fragment>
+      <Header />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
+      <Counter />
+    </Fragment>
   );
 }
 
 export default App;
+// import React from 'react';
+// import Counter from './components/Counter';
+// import Header from './components/Header';
+// import Auth from './components/Auth';
+// import { useSelector } from 'react-redux';
+// import UserProfile from './components/UserProfile'
+
+
+// function App() {
+
+//   const isAuth = useSelector(state => state.auth.isAuthenticated);
+//   console.log(isAuth)
+
+//   return (
+//     <React.Fragment>
+//       <Header/>
+//       {!isAuth && <Auth/>}
+//       {isAuth && <UserProfile/>}
+//       <Counter />
+//     </React.Fragment>
+//   );
+// }
+
+// export default App;
